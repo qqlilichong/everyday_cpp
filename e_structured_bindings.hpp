49f7ed72d772a2f75b3cad7d3b2b4c94bec279ba
ric <qqlilichong@163.com>
2020-04-03 12:29:23 +0800

    update.

--- e_structured_bindings.hpp
+++ e_structured_bindings.hpp
@@ -7,9 +7,16 @@
 
 namespace e_structured_bindings {
 
+    struct myst
+    {
+        int p1 = 0 ;
+        int p2 = 0 ;
+        int p3 = 0 ;
+    };
+
     inline auto ut()
     {
-        auto&& [a, b, c, d] = std::make_tuple(1, 2, 110, 3) ;
+        auto&& [a, b, c, d] = std::make_tuple(1, 2, 3, 4) ;
 
         int result = 0 ;
         std::map<int, int> mp = { {a, b}, {c, d} } ;
@@ -17,6 +24,23 @@ namespace e_structured_bindings {
         {
             result += k ;
             result += v ;
+        }
+
+        myst obj = { 2, 3, 101 } ;
+        auto&& [p1, p2, p3] = obj ;
+        if ( p1 == obj.p1 )
+        {
+            result += obj.p1 ;
+        }
+
+        if ( p2 == obj.p2 )
+        {
+            result += obj.p2 ;
+        }
+
+        if ( p3 == obj.p3 )
+        {
+            result += obj.p3 ;
         }
 
         // expect : 116
