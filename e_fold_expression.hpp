c91f73d9b7fe08251cf53860c33f5d5e58574d71
ric <qqlilichong@163.com>
2020-04-05 19:31:52 +0800

    update.

--- e_fold_expression.hpp
+++ e_fold_expression.hpp
@@ -7,33 +7,33 @@ namespace e_fold_expression {
     template <typename T, typename R>
     auto myadd(T val, R& result)
     {
-        result += val ;
+        result += val;
     }
 
     template <typename R>
     auto myadd(const char* val, R& result)
     {
-        result += std::stoll(val) ;
+        result += std::stoll(val);
     }
 
     template <typename... ARGS>
     auto myadd(ARGS&&... args)
     {
-        decltype(std::stoll("")) result = 0 ;
-        (myadd(std::forward<ARGS>(args), result), ...) ;
-        return result ;
+        decltype(std::stoll("")) result = 0;
+        (myadd(std::forward<ARGS>(args), result), ...);
+        return result;
     }
 
     template <typename... ARGS>
     auto mysum(ARGS&&... args)
     {
-        return (args + ...) ;
+        return (args + ...);
     }
 
     inline auto ut()
     {
         // expect : 116
-        return myadd("1", 2) + myadd(0) + myadd("3") + mysum(100) + mysum(1, 2, 3, 4) ;
+        return myadd("1", 2) + myadd(0) + myadd("3") + mysum(100) + mysum(1, 2, 3, 4);
     }
 }
 
